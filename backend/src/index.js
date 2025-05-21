735a5a4c1992cc10036a37e9a52dc9f9022bd27b
Yuri C.IV <145846514+CRazorIV@users.noreply.github.com>
2025-05-21 20:59:29 +0200

    signup controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js'
-import dotenv from 'dotenv'
-import { connectDB } from './lib/db.js'
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
-    connectDB()
-})
+  console.log(`Server is running on ${port}`);
+  connectDB();
+});
